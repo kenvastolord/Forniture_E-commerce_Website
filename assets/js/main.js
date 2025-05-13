fb2b3efd804df0df4d292077dacacfd483f962c6
Kenneth Delgado <kenvastolord@gmail.com>
2025-05-13 11:06:03 -0600

    Update navbar styles
    - Updated navbar__link styles with pink box shadow and hover effect.
    - Prevented body scrolling when mobile menu is open.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const openMenu = document.getElementById("openMenu");
 const closeMenu = document.getElementById("closeMenu");
 const navbar = document.querySelector("nav");
+const body = document.body
 
 openMenu.classList.add("show");
 
@@ -8,10 +9,21 @@ openMenu.addEventListener("click", () => {
   openMenu.classList.remove("show");
   closeMenu.classList.add("show");
   navbar.classList.add("show");
+  body.classList.add("no-scroll")
 });
 
 closeMenu.addEventListener("click", () => {
   closeMenu.classList.remove("show");
   openMenu.classList.add("show");
   navbar.classList.remove("show");
+  body.classList.remove("no-scroll")
+});
+
+document.querySelectorAll(".navbar__link").forEach((link) => {
+  link.addEventListener("click", () => {
+    navbar.classList.remove("show");
+    closeMenu.classList.remove("show");
+    openMenu.classList.add("show");
+    body.classList.remove("no-scroll");
+  });
 });
